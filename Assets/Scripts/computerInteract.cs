3e5091e209fe79733682569286304c33be8d0a62
GeorgeR23 <gr849@york.ac.uk>
2026-04-16 17:36:07 +0100

    V1
    Created room,
    Added mouse look system,
    Started screens needs improvement.

---
+++ Assets/Scripts/computerInteract.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ComputerInteract : MonoBehaviour
+{
+    public GameObject screenUI;
+    public float interactDistance = 3f;
+
+    private Camera playerCamera;
+    private bool isUsing = false;
+    public MouseLook mouseLook;
+
+    void Start()
+    {
+        playerCamera = Camera.main;
+        mouseLook = Camera.main.GetComponent<MouseLook>();
+    }
+
+    void Update()
+    {
+        if (!isUsing)
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, interactDistance))
+                {
+                    if (hit.transform == transform)
+                    {
+                        OpenComputer();
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Mouse.current.rightButton.wasPressedThisFrame)
+            {
+                CloseComputer();
+            }
+        }
+    }
+
+    void OpenComputer()
+    {
+        screenUI.SetActive(true);
+        mouseLook.enabled = false;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        isUsing = true;
+    }
+
+    void CloseComputer()
+    {
+        screenUI.SetActive(false);
+        mouseLook.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isUsing = false;
+    }
+}
---
+++ Assets/Scripts/mouseLook.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 0.1f;
+    public Transform playerBody;
+
+    private float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Vector2 mouseDelta = Mouse.current.delta.ReadValue();
+
+        float mouseX = mouseDelta.x * sensitivity;
+        float mouseY = mouseDelta.y * sensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    
+}
